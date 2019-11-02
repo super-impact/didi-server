e22431ed282d266262b7d4385ae4b1d2b8e07389
jeffchoi72 <ihello0720@gmail.com>
2019-11-02 23:52:09 +0900

    refactor: post를 join하는 로직을 가독성 좋게 바꾼다

--- src/database/repository/post.repository.ts
+++ src/database/repository/post.repository.ts
@@ -7,13 +7,14 @@ import { Post } from '../entity';
 @EntityRepository(Post)
 class PostRepository extends Repository<Post> {
   public async getPosts({ take, skip }: { take: number; skip: number }) {
-    return this.createQueryBuilder('posts')
-      .leftJoinAndSelect('posts.topics', 'topics')
-      .leftJoinAndSelect('posts.contributorUser', 'contributorUser')
-      .orderBy('posts.createdAt', 'DESC')
-      .skip(skip)
-      .take(take)
-      .getMany();
+    return this.find({
+      relations: ['contributorUser', 'topics'],
+      order: {
+        createdAt: 'DESC',
+      },
+      skip,
+      take,
+    });
   }
 }
 
