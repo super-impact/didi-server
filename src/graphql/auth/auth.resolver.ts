4e235b20b036174e7c705625a083806f405fed96
jeffchoi72 <ihello0720@gmail.com>
2019-11-30 22:06:00 +0900

    feat: sigUp resolver를 만든다

--- src/graphql/auth/auth.resolver.ts
+++ src/graphql/auth/auth.resolver.ts
@@ -1,23 +1,52 @@
 import { Arg, Mutation, Resolver } from 'type-graphql';
 import { Service } from 'typedi';
 
-import { UserService } from '../../service';
+import { AuthService, UserService } from '../../service';
+import { encryptPassword } from '../../utils';
+import { generateGraphQLError, GraphQLErrorMessage } from '../error';
 import { User } from '../user/user.type';
-import { SignInInput, SignUpInput } from './auth.type';
+import { Auth, SignInInput, SignUpInput } from './auth.type';
 
-@Resolver(User)
+@Resolver(Auth)
 @Service()
 class AuthResolver {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private authService: AuthService) {}
 
-  @Mutation(returns => User)
+  @Mutation(returns => Auth)
   async signIn(@Arg('signIn') signIn: SignInInput) {
     return {} as User;
   }
 
-  @Mutation(returns => User)
+  @Mutation(returns => Auth)
   async signUp(@Arg('signUp') signUp: SignUpInput) {
-    return {} as User;
+    const { provider, email, password, displayName, profileImageUrl } = signUp;
+
+    // TODO(hoon): provider에 따라서 로직을 타게한다.
+
+    const isExistedUser = await this.userService.isExistedUserByEmail(email);
+
+    if (isExistedUser) {
+      return generateGraphQLError(GraphQLErrorMessage.ExistEmail);
+    }
+
+    const { passwordSalt, passwordHash } = encryptPassword(password);
+
+    const user = await this.userService.createUser({
+      email,
+      passwordSalt,
+      passwordHash,
+      provider,
+      displayName,
+      profileImageUrl,
+    });
+
+    const { accessToken, refreshToken } = await this.authService.getAuthToken(user);
+
+    return {
+      user,
+      accessToken,
+      refreshToken,
+    };
   }
 }
 
