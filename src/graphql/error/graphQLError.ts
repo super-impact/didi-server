d414d50b9ed865d951818e5dd0d9dd9f07c2fd28
jeffchoi72 <ihello0720@gmail.com>
2019-12-07 22:00:13 +0900

    feat: GraphQLErrorMessage에 NotFoundUser와 NotCorrectPassword를 추가한다

--- src/graphql/error/graphQLError.ts
+++ src/graphql/error/graphQLError.ts
@@ -2,6 +2,8 @@ import { GraphQLError } from 'graphql';
 
 export enum GraphQLErrorMessage {
   ExistEmail = 'EXIST_EMAIL',
+  NotFoundUser = 'NOT_FOUND_USER',
+  NotCorrectPassword = 'NOT_CORRECT_PASSWORD',
 }
 
 export const generateGraphQLError = (message: string) => new GraphQLError(message);
