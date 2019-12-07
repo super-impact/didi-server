c9264f551fc73cccab03f7a201698928e26385f6
jeffchoi72 <ihello0720@gmail.com>
2019-12-07 21:57:12 +0900

    feat: user.service에 isExistedUserByEmail 반환값을 boolean으로 만들고 getUserByEmail 메서드를 추가한다

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -9,11 +9,16 @@ class UserService {
   constructor(@InjectRepository() private readonly userRepository: UserRepository) {}
 
   public async isExistedUserByEmail(email: string) {
-    return this.userRepository.findByEmail(email);
+    const isExistedUser = await this.userRepository.findByEmail(email);
+    return !!isExistedUser;
   }
 
   public async createUser(user: Partial<User>) {
     return this.userRepository.save(user);
+  }
+
+  public async getUserByEmail(email: string) {
+    return this.userRepository.findByEmail(email);
   }
 }
 
