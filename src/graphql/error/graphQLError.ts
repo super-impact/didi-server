5b8ec1a238a7dbc3097b5c481435179deded514c
jeffchoi72 <ihello0720@gmail.com>
2019-12-08 02:35:59 +0900

    feat: GraphQLErrorMessage enum에 different provider를 추가한다

--- src/graphql/error/graphQLError.ts
+++ src/graphql/error/graphQLError.ts
@@ -4,6 +4,9 @@ export enum GraphQLErrorMessage {
   ExistEmail = 'EXIST_EMAIL',
   NotFoundUser = 'NOT_FOUND_USER',
   NotCorrectPassword = 'NOT_CORRECT_PASSWORD',
+  DifferentProvider = 'DIFFERENT_PROVIDER',
 }
 
-export const generateGraphQLError = (message: string) => new GraphQLError(message);
+export const generateGraphQLError = (message: string) => {
+  throw new GraphQLError(message);
+};
