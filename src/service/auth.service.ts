f37a130f134e10925b3a57f08a4d3d14225bebd1
jeffchoi72 <ihello0720@gmail.com>
2019-12-08 00:39:08 +0900

    feat: Auth Service에 google 인증 관련 서비스 로직을 정의한다

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,12 +1,10 @@
 import { Service } from 'typedi';
 
 import { User } from '../database/entity';
-import { generateAccessToken, generateRefreshToken } from '../utils';
+import { generateAccessToken, generateAuthURL, generateRefreshToken, getUserProfileFromCode } from '../utils';
 
 @Service()
 class AuthService {
-  constructor() {}
-
   public async getAuthToken(user: Pick<User, 'id'>) {
     const accessToken = await generateAccessToken({ id: user.id });
     const refreshToken = await generateRefreshToken({ id: user.id });
@@ -14,6 +12,21 @@ class AuthService {
     return {
       accessToken,
       refreshToken,
+    };
+  }
+
+  public getGoogleAuthURL() {
+    return generateAuthURL();
+  }
+
+  public async getUserProfileByGoogle(code: string) {
+    const userProfile = await getUserProfileFromCode(code);
+    const { email, name, picture } = userProfile.data;
+
+    return {
+      email,
+      name,
+      picture,
     };
   }
 }
