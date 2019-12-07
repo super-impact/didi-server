6b9f517e5da443e390292ac686e16a18a0a1e504
jeffchoi72 <ihello0720@gmail.com>
2019-12-08 02:38:32 +0900

    feat: auth.service에 구글 인증을 위한 로직을 추가한다

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,13 +1,26 @@
 import { Service } from 'typedi';
 
-import { User } from '../database/entity';
+import { Provider } from '../graphql/auth/auth.type';
 import { generateAccessToken, generateAuthURL, generateRefreshToken, getUserProfileFromCode } from '../utils';
+import UserService from './user.service';
+
+type User = {
+  id: string;
+  email: string;
+  password: string;
+  displayName: string;
+  profileImageUrl: string;
+};
 
 @Service()
 class AuthService {
+  constructor(private userService: UserService) {}
+
   public async getAuthToken(user: Pick<User, 'id'>) {
-    const accessToken = await generateAccessToken({ id: user.id });
-    const refreshToken = await generateRefreshToken({ id: user.id });
+    const { id } = user;
+
+    const accessToken = await generateAccessToken({ id });
+    const refreshToken = await generateRefreshToken({ id });
 
     return {
       accessToken,
@@ -19,7 +32,25 @@ class AuthService {
     return generateAuthURL();
   }
 
-  public async getUserProfileByGoogle(code: string) {
+  public async getUserByGoogleAuth(oAuthCode: string) {
+    const { email, name, picture } = await this.getUserProfileByGoogle(oAuthCode);
+    const foundUser = await this.userService.getUserByEmail(email);
+
+    if (!foundUser) {
+      const user = await this.userService.createUser({
+        email,
+        displayName: name,
+        profileImageUrl: picture,
+        provider: Provider.Google,
+      });
+
+      return user;
+    }
+
+    return foundUser;
+  }
+
+  private async getUserProfileByGoogle(code: string) {
     const userProfile = await getUserProfileFromCode(code);
     const { email, name, picture } = userProfile.data;
 
