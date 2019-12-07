c56c9a13d2f19ec19460fa94b697c704a3dfb150
jeffchoi72 <ihello0720@gmail.com>
2019-12-08 00:32:19 +0900

    feat: Server에 koa-router를 추가하고 middleware와 route 설정 실행시점을 변경한다

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,10 @@ import { ApolloServer } from 'apollo-server-koa';
 import Cors from 'kcors';
 import Koa from 'koa';
 import BodyParser from 'koa-bodyparser';
+import Router from 'koa-router';
+import Container from 'typedi';
 
+import { Routes } from './controllers';
 import { connectDatabase } from './database/connection';
 import graphqlSchema from './graphql';
 import { logger } from './utils/logger';
@@ -12,17 +15,12 @@ import { logger } from './utils/logger';
 class Server {
   private app: Koa;
   private apolloServer: ApolloServer;
+  private router: Router;
 
   constructor() {
     this.app = new Koa();
+    this.router = new Router();
     this.apolloServer = new ApolloServer({ schema: graphqlSchema });
-
-    this.setMiddlewares();
-    this.setApolloServer();
-  }
-
-  private setApolloServer() {
-    this.apolloServer.applyMiddleware({ app: this.app });
   }
 
   private setMiddlewares() {
@@ -30,8 +28,22 @@ class Server {
     this.app.use(BodyParser());
   }
 
+  private setRoutes() {
+    this.router.use('/api', Container.get(Routes).getRoutes());
+    this.app.use(this.router.routes());
+    this.app.use(this.router.allowedMethods());
+  }
+
+  private setApolloServer() {
+    this.apolloServer.applyMiddleware({ app: this.app });
+  }
+
   public async runServer(port: string) {
     await connectDatabase();
+
+    this.setMiddlewares();
+    this.setRoutes();
+    this.setApolloServer();
 
     this.app.listen(port, () => {
       logger.info(`Server is running on http://localhost:${port}`);
