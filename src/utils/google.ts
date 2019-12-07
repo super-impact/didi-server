352fa80932136b3b5a93fcaacfb6bb7c413cf7b2
jeffchoi72 <ihello0720@gmail.com>
2019-12-08 00:37:53 +0900

    feat: utils에 google api util을 추가한다

---
+++ src/utils/google.ts
@@ -0,0 +1,39 @@
+import { google } from 'googleapis';
+
+const { GOOGLE_CLIENT_ID, GOOGLE_SECRET, GOOGLE_REDIRECT } = process.env;
+
+const googleConfig = {
+  clientId: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_SECRET,
+  redirect: GOOGLE_REDIRECT,
+};
+
+const defaultScope = ['https://www.googleapis.com/auth/userinfo.profile'];
+
+export const googleOAuth2Client = new google.auth.OAuth2(
+  googleConfig.clientId,
+  googleConfig.clientSecret,
+  googleConfig.redirect,
+);
+
+export function generateAuthURL() {
+  return googleOAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: defaultScope,
+  });
+}
+
+export async function getUserProfileFromCode(code: string) {
+  const authData = await googleOAuth2Client.getToken(code);
+
+  const { tokens } = authData;
+
+  googleOAuth2Client.setCredentials(tokens);
+
+  const oauth2 = google.oauth2({
+    auth: googleOAuth2Client,
+    version: 'v2',
+  });
+
+  return oauth2.userinfo.get();
+}
