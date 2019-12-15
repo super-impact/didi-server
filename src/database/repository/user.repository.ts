631a429b08ab58f23e19d2a6f5aea541632d75a4
jeffchoi72 <ihello0720@gmail.com>
2019-12-15 23:32:05 +0900

    feat: user repository에 findById를 추가한다

--- src/database/repository/user.repository.ts
+++ src/database/repository/user.repository.ts
@@ -6,6 +6,10 @@ import { User } from '../entity';
 @Service()
 @EntityRepository(User)
 class UserRepository extends Repository<User> {
+  public findById(id: string) {
+    return this.findOne({ id });
+  }
+
   public findByEmail(email: string) {
     return this.findOne({ email });
   }
