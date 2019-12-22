b1c30612bb2e15648690874e7a3ae8aaf27ca86e
jeffchoi72 <ihello0720@gmail.com>
2019-12-23 00:19:53 +0900

    chore: 사용하지 않는 eslint 옵션을 끈다

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
   },
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/camelcase': 'off',
   },
 };
